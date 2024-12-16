a86f4d3527c24b00a4b0d28825f79e065129b429
Samuel <adeoyesamuel100@gmail.com>
2024-12-16 11:38:27 +0100

    update using pycodestyle style

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,7 +2,8 @@
 
 '''
 
-Create a class BasicCache that inherits from BaseCaching and is a caching system:
+Create a class BasicCache that inherits
+from BaseCaching and is a caching system:
 
 You must use self.cache_data - dictionary from the parent class BaseCaching
 This caching system doesn’t have limit
@@ -16,6 +17,7 @@ If key is None or if the key doesn’t exist in self.cache_data, return None.
 '''
 
 BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
     """
