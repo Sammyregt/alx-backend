216a9589337e3e2397ff6c051f7dac8ae6f71db4
Samuel <adeoyesamuel100@gmail.com>
2024-12-16 12:58:48 +0100

    update files

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -50,14 +50,15 @@ class FIFOCache(BaseCaching):
 
         """
         if key is None or item is None:
-            pass
-        else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                first_key = next(iter(self.cache_data.keys()))
-                del self.cache_data[first_key]
-                print("Discard {}".format(first_key))
+            return
+        
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                and key not in self.cache_data:
+            first_key = next(iter(self.cache_data))
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
 
-            self.cache_data[key] = item
+        self.cache_data[key] = item
 
     def get(self, key):
         """return the value in self.cache_data linked to key
